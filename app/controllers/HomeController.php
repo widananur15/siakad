6b49bc6781a28a92934d584277ea6c78db91e72a
sts <widana@solusiteknologi.co.id>
2015-07-31 21:27:34 +0700

    commit

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -15,9 +15,9 @@ class HomeController extends BaseController {
 	|
 	*/
 
-	public function showWelcome()
+	public function showformlogin()
 	{
-		return View::make('hello');
+		return View::make('login/login');
 	}
 
 }
