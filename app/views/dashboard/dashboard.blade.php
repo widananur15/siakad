689ba1aa7a7a5ed3a91ba04f386cfaab361ea4e1
sts <widana@solusiteknologi.co.id>
2015-08-04 11:54:22 +0700

    *

---
+++ app/views/dashboard/dashboard.blade.php
@@ -0,0 +1,16 @@
+@extends('layout/default')
+@section('head')
+
+        <title>SMK Bagimu Negeriku</title>
+@stop
+
+
+
+@section('content')
+<div class="col-lg-12">
+                <h1 class="page-header">Dashboard</h1>
+
+
+</div>
+
+@stop
