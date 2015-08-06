b803b3586590061710819ce6df9ff9329e164997
chrismerrari <chrismerrari78@gmail.com>
2015-08-06 17:09:48 +0700

    *

---
+++ app/views/mapel/add_mapel.blade.php
@@ -0,0 +1,57 @@
+@extends('...layout.default')
+@section('head')
+
+<title>Add Mapel</title>
+
+@stop
+
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">Add Mapel</h1>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+
+<a href="/mapel"><p class="fa fa-arrow-left"> BACK </p></a>
+            </div>
+            <!-- /.panel-heading -->
+            <div class="panel-body">
+
+
+                <form action="mapel/add/new" method="post" class="form-group" >
+
+                <br>
+                <h3 class=""> Add Mapel</h3>
+                {{ Form::label("","Mapel Name") }}
+                <input type="text" name="nama_mapel" class="form-control" placeholder="Nama Mapel" >
+
+                {{ Form::label("","Active") }}
+
+                {{ Form::select('active',$active,Input::get('active'),['class'=>'form-control'] ) }}
+
+
+
+
+                <br>
+                <br>
+                <br>
+                <br>
+                <div class="col-sm-6">
+               <input type="reset" value="Back" class="btn btn-warning">
+                <input type="submit" value="Add" class="btn btn-primary">
+                   </div>
+
+                </form>
+
+
+
+
+            </div>
+            <!-- /.panel-body -->
+        </div>
+        <!-- /.panel -->
+    </div>
+</div>
+
+@stop
---
+++ app/views/mapel/edit_mapel.blade.php
@@ -0,0 +1,53 @@
+@extends('...layout.default')
+@section('head')
+
+<title>Management Student</title>
+
+@stop
+
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">Edit Mapel</h1>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+
+            </div>
+            <!-- /.panel-heading -->
+            <div class="panel-body">
+
+
+                {{Form::model($mapel,array('route' => array('mapel.update',$mapel->mapel_id), 'method' => 'put','class'=>'form-horizontal'))}}
+
+                <br>
+                <h3 class=""> Edit</h3>
+                {{ Form::label("","Mapel ID") }}
+                <input disabled type="text" name="mapel_id" class="form-control" placeholder="ID" value="{{$edit->mapel_id}}">
+                {{ Form::label("","Mapel Name") }}
+                <input type="text" name="nama_mapel" class="form-control" placeholder="Nama Mapel" value="{{$edit->nama_mapel}}">
+                {{ Form::label("","Active") }}
+
+                {{Form::select('active',$active,Input::old('active'),['class'=>'form-control','value'=>'$edit->active']) }}
+
+
+
+
+
+                <br>
+                <br>
+                <br>
+                <br>
+                <input type="submit" value="Edit" class="btn btn-primary">
+                </form>
+
+
+
+            </div>
+            <!-- /.panel-body -->
+        </div>
+        <!-- /.panel -->
+    </div>
+</div>
+
+@stop
